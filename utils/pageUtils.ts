4a1c6b03ccf1e7c5f34b749f49a26bfd6fbf36bb
Cristi-Fogel <37100222+Cristi-Fogel@users.noreply.github.com>
2025-02-26 08:48:33 +0200

    add waitForElement function to pageUtils file

--- utils/pageUtils.ts
+++ utils/pageUtils.ts
@@ -1,6 +1,10 @@
-import { Page } from '@playwright/test';
+import { Page, Locator } from '@playwright/test';
 
 // used primarily for frontend tests
 export async function navigateTo(page: Page, url: string) {
     await page.goto(url);
+}
+
+export async function waitForElement(page: Page, locator: Locator, timeout = 5000) {
+    await locator.waitFor({ state: 'visible', timeout });
 }
