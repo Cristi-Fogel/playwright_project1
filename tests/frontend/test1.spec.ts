88de450331481b3976aefc78048f7b964915f336
Cristi-Fogel <37100222+Cristi-Fogel@users.noreply.github.com>
2025-04-16 08:54:09 +0300

    remove redundant login function from test

--- tests/frontend/test1.spec.ts
+++ tests/frontend/test1.spec.ts
@@ -6,30 +6,27 @@ import { FrontentHerokuLandingPage } from '../../pageObjects/frontentHerokuLandi
 import { validationStrings } from '../../validationStrings';
 import { credentials } from '../../baseCredentials';
 
-async function login(page, loginPage, username, password) {
-  await navigateTo(page, getURL("herokuURL", "loginPageHeroku"));
-  await loginPage.login(username, password);
-}
-
 test('frontend - has title', async ({ page }) => {
   await navigateTo(page, getURL("herokuURL", "loginPageHeroku"));
   await expect(page).toHaveTitle("The Internet");
 });
 
-test('frontend Heroku - Login test', async ({page})=>{
+test('frontend Heroku - Login test', async ({ page }) => {
   const loginPage = new FrontendHerokuLoginPage(page);
   const landingPage = new FrontentHerokuLandingPage(page);
 
-  await login(page, loginPage, credentials.users.admin.username, credentials.users.admin.password);
+  await navigateTo(page, getURL("herokuURL", "loginPageHeroku"));
+  await loginPage.login(credentials.users.admin.username, credentials.users.admin.password);
   const loginMessage = await landingPage.getLoginMessage();
   await expect(loginMessage).toBe(validationStrings.login.successMessage);
 });
 
-test('frontend Heroku - Logout test', async ({page})=>{
+test('frontend Heroku - Logout test', async ({ page }) => {
   const loginPage = new FrontendHerokuLoginPage(page);
   const landingPage = new FrontentHerokuLandingPage(page);
 
-  await login(page, loginPage, credentials.users.admin.username, credentials.users.admin.password);
+  await navigateTo(page, getURL("herokuURL", "loginPageHeroku"));
+  await loginPage.login(credentials.users.admin.username, credentials.users.admin.password);
   await landingPage.logoutButton.click();
   const logoutMessage = await loginPage.getLogoutSuccessMessage();
   await expect(logoutMessage).toContain(validationStrings.logout.successMesage);
